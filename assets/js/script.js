b0d99cd6802eb3c5682419c906361dca0ca81a81
Rakesh Chowdhury <67048804+rakeshid03@users.noreply.github.com>
2023-05-11 07:08:49 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ setTimeout(function() {
 
     PetImage.src = "./assets/loading.gif";
 
-  }, 50);
+  }, 100);
 }
 closeButton.onclick = PetContainer.onclick = closeModel;
 // prevent the PetContainer from closing when the pet-card is clicked
